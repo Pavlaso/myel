63f47cc60f6017efb32ba3d002f45d9a1c10d4de
Pavlaso <pavlaso25@mail.ru>
2022-07-06 13:49:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll:1,
+    prevArrow: '<img class="slider-arrows categories-slider__arrow-left" src="img/arrow-left.svg" alt="arrow"></img>',
+    nextArrow: '<img class="slider-arrows categories-slider__arrow-right" src="img/arrow-right.svg" alt="arrow"></img>',
+});
+
+$('.ornam-slider__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll:1,
+    prevArrow: '<img class="slider-arrows-orn categories-slider__arrow-left" src="img/arrow-left.svg" alt="arrow"></img>',
+    nextArrow: '<img class="slider-arrows-orn categories-slider__arrow-right" src="img/arrow-right.svg" alt="arrow"></img>',
+});
+
+
+
